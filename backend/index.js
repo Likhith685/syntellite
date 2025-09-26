4502c5e682410a77ee33a2bc1b00130ef9a30fe7
Likhithraj <likhith685@gmail.com>
2025-09-26 23:42:44 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 // ------------------------
 app.use(
   cors({
-    origin: "https://syntellite-a17n.vercel.app",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
